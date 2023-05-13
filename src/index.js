eee6cc72ebbc77ef41c51832574699ab5de9ed08
anasmuostafa <anasmuostafa@gmail.com>
2023-05-13 05:52:47 +0300

    adding layout + dependencies

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles/normalize.css";
+
+// Import our custom CSS
+import "./scss/styles.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+console.log(" to do");
