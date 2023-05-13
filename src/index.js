70a107ab9c7877ff899a713fefb822cb6ea144bf
anasmuostafa <anasmuostafa@gmail.com>
2023-05-13 06:45:51 +0300

    adding header and footer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { footer } from "./script/footer";
+import { header } from "./script/header";
 import "./styles/normalize.css";
 
 // Import our custom CSS
@@ -5,5 +7,11 @@ import "./scss/styles.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+const content = document.querySelector("#content");
+
+content.appendChild(header);
+
+content.appendChild(footer);
 
 console.log(" to do");
